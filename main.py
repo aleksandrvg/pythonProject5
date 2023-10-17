01cec33db8e53c41e43379746fb1a8e3ae2d2949
student <aleksandrvg79@gmail.com>
2023-10-17 20:31:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+array = range(10)
+array = list(map(lambda x: x ** 2, array))
+print(array)
+
+array = range(10)
+array = list(filter(lambda x: x % 2 == 0, array))
+print(array)
+
+array = range(1, 10)
+array = reduce(lambda x, y: x * y, array, 1)
+print(array)
+
+with open("test.txt", "w", encoding= "utf-8") as file:
+    file.write("Hello world")
+with open("test.txt", "r", encoding= "utf-8") as file:
+    print(file.read())
