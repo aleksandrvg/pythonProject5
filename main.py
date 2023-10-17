0bbf462f78e87752f950ff27ee13dee13e8a5609
student <aleksandrvg79@gmail.com>
2023-10-17 21:39:00 +0300

    Commit 2

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ array = range(1, 10)
 array = reduce(lambda x, y: x * y, array, 1)
 print(array)
 
-with open("test.txt", "w", encoding= "utf-8") as file:
+with open("test.txt", "w", encoding="utf-8") as file:
     file.write("Hello world")
-with open("test.txt", "r", encoding= "utf-8") as file:
+with open("test.txt", "r", encoding="utf-8") as file:
     print(file.read())
+
+with open("некийтекстовый файл.txt", "r", encoding="utf-8") as file:
+    array = file.read()
+    array = list(filter(lambda x: str.isdigit(x), array))
+    print(array)
+    print(len(array))
